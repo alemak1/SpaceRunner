9cf4dc649e027b8d98fe1a64097dfcf2cd6ae520
yongrenwang <czie.alexmak@gmail.com>
2017-02-22 15:47:24 +0800

    Added a class for the background

---
+++ SpaceRunner/SpaceRunner/SpaceRunner/Background.swift
@@ -0,0 +1,13 @@
+//
+//  Background.swift
+//  SpaceRunner
+//
+//  Created by Aleksander Makedonski on 2/22/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import SpriteKit
+
+class Background: SKNode {
+
+}
