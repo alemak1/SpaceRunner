da568295b6eafcd7fad0a734b4a8650863671d7a
yongrenwang <czie.alexmak@gmail.com>
2017-02-22 14:53:47 +0800

    Customized and implemented a class for the PlayButton on the menu

---
+++ SpaceRunner/SpaceRunner/SpaceRunner/PlayButton.swift
@@ -0,0 +1,31 @@
+//
+//  PlayButton.swift
+//  SpaceRunner
+//
+//  Created by Aleksander Makedonski on 2/22/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+
+class PlayButton: SKSpriteNode {
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    
+    override init(texture: SKTexture?, color: UIColor, size: CGSize){
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    convenience init() {
+        let texture = GameTextures.sharedInstance.sprite(name: SpriteName.playButton) //Replace with code to get a texture from the atlas
+        self.init(texture: texture, color: SKColor.white, size: texture.size())
+        setup()
+    }
+    
+    
+    private func setup(){
+        self.position = CGPoint(x: kViewSize.width/2, y: kViewSize.height*0.30)
+    }
+}
