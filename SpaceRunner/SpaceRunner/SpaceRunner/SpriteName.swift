542c9b2363e36ec40698f1baca59d349b071bb67
yongrenwang <czie.alexmak@gmail.com>
2017-02-22 14:44:58 +0800

    Added a new utility class for names of SpriteNodes; these names are accessed as static variables on the class

---
+++ SpaceRunner/SpaceRunner/SpaceRunner/SpriteName.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteName.swift
+//  SpaceRunner
+//
+//  Created by Aleksander Makedonski on 2/22/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+
+class SpriteName{
+    //MenuScene
+    class var logo: String { return "GameLogo" }
+    class var title: String { return "GameTitle" }
+    class var playButton: String { return "PlayButton" }
+    
+}
