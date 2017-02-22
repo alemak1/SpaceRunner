ded02802d59d727724f9aa0c8a619b60d4c297c3
yongrenwang <czie.alexmak@gmail.com>
2017-02-22 15:41:23 +0800

    Finished configuring the settings for the Background SKEMitterNode

--- SpaceRunner/SpaceRunner/SpaceRunner/GameParticles.swift
+++ SpaceRunner/SpaceRunner/SpaceRunner/GameParticles.swift
@@ -27,7 +27,37 @@ class GameParticles{
     }
     
     private func setupBackgroundEmitter(){
+        //Birthrate and Lifetime
+        backgroundEmitter.particleBirthRate = 25.0
+        backgroundEmitter.particleLifetime = 5.0
+        backgroundEmitter.particleLifetimeRange = 5.0
         
+        //Position Range
+        backgroundEmitter.position = CGPoint(x: kViewSize.width/2, y: kViewSize.height)
+        backgroundEmitter.particlePositionRange = CGVector(dx: kViewSize.width, dy: kViewSize.height*2)
+        
+        //Speed
+        backgroundEmitter.particleSpeed = 200.0
+        backgroundEmitter.particleSpeedRange = backgroundEmitter.particleSpeed/4
+        
+        //Emission Angle
+        backgroundEmitter.emissionAngle = DegreesToRadians(degrees: -90.0)
+        backgroundEmitter.emissionAngleRange = DegreesToRadians(degrees: 15)
+        
+        //Alpha
+        backgroundEmitter.particleAlpha = 0.5
+        backgroundEmitter.particleAlphaRange = 0.25
+        backgroundEmitter.particleAlphaSpeed = -0.125
+        
+        //Color Blending
+        backgroundEmitter.particleColorBlendFactor = 0.5
+        backgroundEmitter.particleColorBlendFactorRange = 0.25
+        
+        //Color
+        backgroundEmitter.particleColor = Colors.colorFromRGB(rgbValue: Colors.dust)
+        
+        //Texture
+        backgroundEmitter.particleTexture = GameTextures.sharedInstance.texture(name: SpriteName.magic)
     }
     
     
