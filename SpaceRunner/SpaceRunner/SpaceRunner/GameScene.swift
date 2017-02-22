87295e9bba77fb7243060caf6714484059191c47
yongrenwang <czie.alexmak@gmail.com>
2017-02-22 12:06:21 +0800

    Finished adding placeholder transitions for scenes

--- SpaceRunner/SpaceRunner/SpaceRunner/GameScene.swift
+++ SpaceRunner/SpaceRunner/SpaceRunner/GameScene.swift
@@ -10,6 +10,9 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    var sceneLabel: SKLabelNode?
+    
     //MARK: - Init
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -27,16 +30,35 @@ class GameScene: SKScene {
     //MARK: - Setup
     private func setup(){
         self.backgroundColor = SKColor.black
+        
+        sceneLabel = SKLabelNode(fontNamed: "Arial")
+        sceneLabel?.fontColor = SKColor.white
+        sceneLabel?.text = "Game currently in play..."
+        sceneLabel?.position = CGPoint(x: kViewSize.width/2, y: kViewSize.height/2)
+        self.addChild(sceneLabel!)
     }
     
     //MARK: - Update
     override func update(_ currentTime: TimeInterval) {
-        
     }
     
     //MARK: - Touch Handling
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch = touches.first! as UITouch
+        let touchLocation = touch.location(in: self)
+        
+        if(sceneLabel!.contains(touchLocation)){
+            loadScene()
+        }
         
     }
+    
+    //MARK: - Load Scene
+    private func loadScene(){
+        let scene = GameOverScene(size: kViewSize)
+        let transition = SKTransition.fade(with: SKColor.black, duration: 0.2)
+        self.view?.presentScene(scene, transition: transition)
+    }
+    
    
 }
