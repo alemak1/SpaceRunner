c48039ceee673d71e2f6a6e61d102e0c78ecf9de
yongrenwang <czie.alexmak@gmail.com>
2017-02-22 14:50:59 +0800

    Implemented and customized the GameTitle Class

---
+++ SpaceRunner/SpaceRunner/SpaceRunner/GameTitle.swift
@@ -0,0 +1,33 @@
+//
+//  GameTitle.swift
+//  SpaceRunner
+//
+//  Created by Aleksander Makedonski on 2/22/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameTitle: SKSpriteNode {
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    
+    override init(texture: SKTexture?, color: UIColor, size: CGSize){
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+    convenience init() {
+        let texture = GameTextures.sharedInstance.texture(name: SpriteName.title) //Replace with code to get a texture from the atlas
+        
+        self.init(texture: texture, color: SKColor.white, size: texture.size())
+        setup()
+    }
+    
+    
+    private func setup(){
+        self.position = CGPoint(x: kViewSize.width/2, y: kViewSize.height*0.7)
+    }
+}
